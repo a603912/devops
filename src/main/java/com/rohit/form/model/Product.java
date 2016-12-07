88a82205c1618e555431e540c8819a6b6d163ef6
rohit gupta <rohitoist@yahoo.com>
2016-12-07 15:21:38 +0530

    added comments

--- src/main/java/com/rohit/form/model/Product.java
+++ src/main/java/com/rohit/form/model/Product.java
@@ -1,37 +1,85 @@
 package com.rohit.form.model;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Product.
+ *
+ * @author a603912
+ */
 public class Product {
 
+	/** The id. */
 	private Integer id;
+	
+	/** The product name. */
 	private String productName;
+	
+	/** The product price. */
 	private Integer productPrice;
+	
+	/** The product price 2. */
 	private Integer productPrice2;
 
 
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
 	public Integer getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the new id
+	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
+	/**
+	 * Gets the product name.
+	 *
+	 * @return the product name
+	 */
 	public String getProductName() {
 		return productName;
 	}
 
+	/**
+	 * Sets the product name.
+	 *
+	 * @param productName the new product name
+	 */
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
 
+	/**
+	 * Gets the product price.
+	 *
+	 * @return the product price
+	 */
 	public Integer getProductPrice() {
 		return productPrice;
 	}
 
+	/**
+	 * Sets the product price.
+	 *
+	 * @param productPrice the new product price
+	 */
 	public void setProductPrice(Integer productPrice) {
 		this.productPrice = productPrice;
 	}
 	
+	/**
+	 * Checks if is new.
+	 *
+	 * @return true, if is new
+	 */
 	public boolean isNew() {
 		return (this.id == null);
 	}
